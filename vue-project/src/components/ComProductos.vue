e59564496e0f636ee8dc60c406be74adcb821025
SergioAndresG <sergiogarcia3421@gmail.com>
2025-03-04 04:06:57 -0500

    Barra de Busqueda, modificaion de estilos de cards, nueva ventana emergente con info

--- vue-project/src/components/ComProductos.vue
+++ vue-project/src/components/ComProductos.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import Swal from 'sweetalert2';
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import axios from 'axios';
 import ComImagen from './icons/IMGENES/ComImagen.vue';
 import ComAgregarP from './ComAgregarP.vue';
@@ -8,13 +8,22 @@ import ComActP from './ComActP.vue';
 import ComConsulPro from './ComConsulPro.vue';
 import { useRouter } from 'vue-router';
 import ComConsulProPre from './ComConsulProPre.vue';
+// Libreria para el icono de busqueda
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 
-
-
+// Variable para almacenar datos de la busqueda de productos
+const searchTerm = ref("")
+// Variables para almacenar los datos traidos por el endpoint
 const productos = ref([]);
 const empleado = ref([]);
+// Contraseña obtenida de la informacion proporcionada por el endpoint
 const contraseñaProporcionada = ref("");
+
+//Variables reactivas para la carta emergente con informacion del producto
+const emergenteMasInformacion = ref(false);
+const productoSeleccionado = ref({})
 
 const cargarEmpleados = async () => {
   try {
@@ -86,10 +95,29 @@ const eliminarProducto = async (idProducto: number) => {
 
 };
 
+const filtrarProductos = computed (()=> {
+  if(!searchTerm.value.trim()) return productos.value;
+
+  const term =searchTerm.value.toLowerCase().trim()
+  return productos.value.filter(pro => 
+    pro.nombre.toLowerCase().includes(term)
+  )
+}) 
+
 
 const ActualizarProducto = ref(false);
 const AgregarProducto = ref(false);
 const ConsultarProducto = ref(false);
+
+
+const openEmergente = (producto) => {
+  emergenteMasInformacion.value = true; // Gurada el producto seleccionado
+  productoSeleccionado.value = producto; // Para la informacion del producto selecionado
+}
+
+const closeEmergente = () => {
+  emergenteMasInformacion.value = false;
+}
 
 const openAct = () => {
   ActualizarProducto.value = true;
@@ -152,33 +180,75 @@ cargarProductos();
   <ComAgregarP v-if="AgregarProducto" @close="closeAdd"/>
   <ComActP v-if="ActualizarProducto" @close="closeAct"/>
 
-  <div class="product-container">
-  <div class="card2" v-for="producto in productos" :key="producto.id">
+  <div class="search-bar">
+    <input 
+      type="text" 
+      v-model="searchTerm" 
+      placeholder="Buscar Productos..." 
+      class="search-input"
+      style="font-family: 'Jura', sans-serif;;"
+    >
+    <font-awesome-icon :icon="faSearch" class="search-icon"/>
+
+  </div>
+    
+
+  <transition-group name="fade" tag="div" class="product-container">
+  <div class="card2" v-for="producto in filtrarProductos" :key="producto.id">
     <div class="product-image">
       <img v-if="producto.ruta_imagen" :src="`http://127.0.0.1:8000/productos/${producto.ruta_imagen}`" class="product-image" />
         <span v-else>{{ producto.imagen }}</span>
     </div>
-    <p>ID: <span class="product-id">{{ producto.id }}</span></p>
     <div class="product-info">
-      <p>Nombre: <span class="product-name">{{ producto.nombre }}</span></p>
-      <p>Tipo: <span class="product-description">{{ producto.tipo }}</span></p>
-      <p>Cantidad: <span class="product-quantity">{{ producto.cantidad }}</span></p>
-      <p>Precio: <span class="product-price">${{ producto.precio_unitario }}</span></p>
-      <p>Categoría: <span class="product-price">{{ producto.categoria }}</span></p>
+        <div class="info-left">
+          <p>Producto: <span class="product-name"> {{ producto.nombre }}</span></p>
+          <p>Cantidad: <span class="product-quantity">{{ producto.cantidad }}</span></p>
+        </div>
+        <div class="info-right">
+          <p>Precio: <span class="product-price">${{ producto.precio_unitario }}</span></p>
+          <p>Categoría: <span class="product-price">{{ producto.categoria }}</span></p>
+        </div>  
     </div>
     <div class="product-buttons">
       <button class="delete-button" @click="eliminarProducto(producto.id)">
         Eliminar
       </button>
     </div>
+     <button @click="openEmergente(producto)" class="card-button">Mas Informacion</button>
+    </div>
+
+    <div v-if="filtrarProductos.length === 0" class="no-results">
+          No se encontraron ingredientes que coincidan con la búsqueda.
+        </div>
+  </transition-group>
+
+  <div v-if="emergenteMasInformacion" class="moreInfo" @click.self="closeEmergente"> <!-- .self se para que si se da click por accidente no se cierre la ventana -->
+    <div class="cardInfo">
+        <div class="moreInfoImage">
+          <img v-if="productoSeleccionado.ruta_imagen" :src="`http://127.0.0.1:8000/productos/${productoSeleccionado.ruta_imagen}`" class="product-image" />
+        </div>
+        <div class="container-info">
+          <p>ID: <span class="product-id">{{ productoSeleccionado.id }}</span></p>
+            <p>Producto: <span class="product-name"> {{ productoSeleccionado.nombre }}</span></p>
+            <p>Tipo: <span class="product-description">{{ productoSeleccionado.tipo }}</span></p>
+            <p>Cantidad: <span class="product-quantity">{{ productoSeleccionado.cantidad }}</span></p>
+            <p>Precio: <span class="product-price">${{ productoSeleccionado.precio_unitario }}</span></p>
+            <p>Categoría: <span class="product-price">{{ productoSeleccionado.categoria }}</span></p>
+            <p>Stock Minimo: <span class="product-price">{{ productoSeleccionado.stock_minimo }}</span></p>
+      </div>
+
+      <button @click="closeEmergente" class="closeMore">Cerrar</button>
+    </div> 
   </div>
-</div>
 
 
 </template>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Jura:wght@700&display=swap');
+
+
+
 header {
     padding: 20px;
     background-color: #000000;
@@ -236,8 +306,8 @@ header {
   }
   
   hr {
-    border-top: 2px solid #D9AB23; /* Ajusta el grosor y color según tus necesidades */
-    margin: 20px 0; /* Ajusta el margen superior e inferior */
+    border-top: 2px solid #D9AB23; 
+    margin: 20px 0; 
     margin-top: 54px;
     width: 1300px;
     margin-left: 7%;
@@ -283,6 +353,22 @@ header {
     background-color: #885b34;
 }
 
+/* Estilos de la barra de busqueda */
+.search-input{
+  margin-left: 32.8%;
+  height: 30px;
+  width: 520px;
+  margin-top: 45px;
+}
+.search-input:focus {
+  outline: none;  
+  border: 1px solid #D9AB23; 
+  box-shadow: 0 0 5px rgba(78, 141, 166, 0.5);  
+  transition: ease-in-out 0.5s;
+}
+.search-icon{
+  margin-left: 15px;
+}
 
 .product-container {
   width: 1000px;
@@ -298,7 +384,8 @@ header {
 .card2 {
   font-family: 'Jura', sans-serif;
   width: 320px;
-  background-color: #a65814ac;
+  height: 480px;
+  background-color: #a65814cc;
   color: white;
   border-radius: 12px;
   text-align: left;
@@ -306,7 +393,21 @@ header {
   padding: 19px;
   transition: transform 0.2s ease-in-out;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  border: 1px solid;
+  border: 1px solid rgb(72, 65, 65);
+}
+
+.fade-enter-active, .fade-leave-active{
+  transition: opacity 0.8s ease-out, transform 0.5s ease-out;
+}
+
+.fade-enter-from, .fade-leave-to {
+  opacity: 0;
+  transform: translateY(-15px) scale(0.95);
+}
+
+.fade-leave-to{
+  opacity: 0;
+  transform: translateY(15px) scale(0.95);
 }
 
 .card2:hover {
@@ -314,22 +415,61 @@ header {
   border-radius: 20px;
 }
 
-.product-image {
-  width: 60%;
-  height: 150px;
-  border-radius: 10%;
-  overflow: hidden;
-  margin-left: 10%;
+.card-button {
+ transform: translate(-50%, 125%);
+ width: 60%;
+ border-radius: 1rem;
+ border: none;
+ background-color: #D9AB23;
+ color: #fff;
+ font-size: 1rem;
+ padding: .5rem 1rem;
+ position: absolute;
+ left: 50%;
+ bottom: 0;
+ opacity: 0;
+ transition: 0.3s ease-out;
 }
 
-.product-image img {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
+.card2:hover {
+ border-color: #ff5900;
+ box-shadow: 0 4px 18px 0 rgba(0, 0, 0, 0.25);
+}
+
+/*boton deespues de hacer hover*/
+.card2:hover .card-button {
+ transform: translate(-50%, 50%);
+ opacity: 1;
+}
+
+.product-image {
+  width: 90%;
+  height: 200px;
+  border-radius: 10%;
+  overflow: hidden;
+  margin-left: 5.5%;
 }
 
 .product-info {
-  margin-top: 15px;
+  display: flex;
+  justify-content: space-between;
+  margin-left: 25px;
+}
+
+.product-info p{
+  font-size: 20px;
+}
+
+.product-info span{
+  font-size: 15px;
+}
+
+.info-left, .info-right {
+  width: 50%;
+}
+
+.info-left p, .info-right p {
+  margin: 20px;
 }
 
 .delete-button {
@@ -349,25 +489,86 @@ header {
 }
 
 .product-name {
-    color: blue;
     color: rgb(255, 255, 255);
-}
-
-.product-id {
-  margin-top: 300px;
+    display: block;
 }
 
 .product-description {
     color: rgb(255, 255, 255);
+    display: block;
+
 }
 
 .product-quantity {
     color: rgb(248, 247, 244);
+    display: block;
+
 }
 
 .product-price {
     color: rgb(255, 255, 255);
     font-size: 1.1em;
+    display: block;
+
+}
+
+.moreInfo{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  z-index: 999;
+
+}
+
+.cardInfo {
+  background: rgb(0, 0, 0);
+  padding: 20px;
+  border-radius: 10px;
+  width: 800px;
+  height: 500px;
+  text-align: center;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.2);
+  border: solid 1px #fff;
+  font-family: 'Jura', sans-serif;
+}
+
+.container-info{
+  height: 200px;
+  width: 200px;
+  margin-left: 520px;
+  margin-top: -410px;
+}
+
+.moreInfoImage img{
+  background-color: #A65814;
+  width: 400px;
+  margin-top: 40px;
+  height: 400px;
+  margin-left: -350px;
+}
+
+.closeMore{
+  background-color: #a41010;
+  color: rgb(255, 255, 255);
+  border: none;
+  padding: 10px 20px;
+  border-radius: 5px;
+  cursor: pointer;
+  font-size: 16px;
+  margin-top: 15px;
+  margin-left: 100px;
+  margin-top: 220px;
+}
+.closeMore:hover{
+  transform: scale(1.1);
+  transition: ease-out 0.3s;
+  background-color: #b14a33;
 }
 
 </style>
